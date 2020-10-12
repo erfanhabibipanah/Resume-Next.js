77387a42bb53294010d328fb5b34153a94cabb5b
erfanhabibipanah <erfanhabibipanah@gmail.com>
2020-10-13 00:13:10 +0330

    fix

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home({ allPostsData }) {
       <Head>…</Head>
       <section className={utilStyles.headingMd}>
         <br />
-        <span>I’m a full-Stack developer and experienced in React.js, Next.js, React-Redux, JavaScript, Node.js, Python, Django, HTML, CSS, Sass, MongoDB, Wordpress, TypeScript,  C, C++, C#.</span>
+        <span>I’m a Full-Stack developer and experienced in React.js, Next.js, React-Redux, JavaScript, Node.js, Python, Django, HTML, CSS, Sass, MongoDB, Wordpress, TypeScript,  C, C++, C#.</span>
         <br />
         <br />
         <span>I have experienced in web designing(UI/UX) and know Adobe Photoshop and Adobe Illustrator .</span>
