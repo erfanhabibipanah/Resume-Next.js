dee287e6c3fea32952ce1aee3ced8961785b6add
erfanhabibipanah <erfanhabibipanah@gmail.com>
2020-10-18 21:33:32 +0330

    google analystics add

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
 import "../styles/index.css";
 import "../styles/global.css";
+import Analysis from "../components/analysis"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Analysis>
+      <Component {...pageProps} />
+    </Analysis>
+    );
 }
 
 export default MyApp;
