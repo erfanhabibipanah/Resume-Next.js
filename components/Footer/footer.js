9800bcc33d2313e46007b06a7b54c5e3b491500a
erfanhabibipanah <erfanhabibipanah@gmail.com>
2020-11-09 00:29:40 +0330

    github svg add

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Linkedin from "../../public/images/svg/linkedin.svg";
+import GitHub from "../../public/images/svg/github.svg";
 import footerStyle from "./footer.module.css";
 
 const Footer = () => {
@@ -15,19 +16,27 @@ const Footer = () => {
       >
         <div
           class="flex pb-5 px-3 m-auto pt-5 text-gray-800 pt-8 mb-5 flex-col
-      flex-row max-w-6xl text-center grid grid-cols-10 xl:gap-10 lg:gap-10 md:gap-10 sm:gap-5"
+      flex-row max-w-6xl text-center grid grid-cols-11 xl:gap-10 lg:gap-10 md:gap-10 sm:gap-5"
         >
           <div class="text-center col-start-2 col-span-8">
             © Erfan Habibi Panah Fard. All Rights Reserved.
           </div>
-          <div class={`flex col-end-11 col-span-1 ${footerStyle.linkedin}`}>
+          <div class={"flex col-end-12 col-span-2"}>
             <a
               rel="preconnect"
               href="https://www.linkedin.com/in/erfanhabibipanah"
               target="_blank"
-              className="w-6 mx-1"
+              className={`w-6 mx-1 ${footerStyle.a}`}
             >
               <Linkedin />
+            </a>
+            <a
+              rel="preconnect"
+              href="https://github.com/erfanhabibipanah"
+              target="_blank"
+              className={`w-6 mx-1 ${footerStyle.a}`}
+            >
+              <GitHub />
             </a>
           </div>
         </div>
