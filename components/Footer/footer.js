1f05f3efff9c816f9e5b68972772740d52096242
erfanhabibipanah <erfanhabibipanah@gmail.com>
2020-11-16 16:07:11 +0330

    footer icon

--- components/Footer/footer.js
+++ components/Footer/footer.js
@@ -8,46 +8,41 @@ const Footer = () => {
   return (
     <div>
       <div
-        className="bg-gray-100 z-1 border-t"
+        className="bg-gray-100 z-1 border-t pb-5 px-3 m-auto pt-5 text-gray-800 pt-8 grid grid-column "
         style={{
           width: "100%",
-          display: "inline-flex",
           flexFlow: "column nowrap",
         }}
       >
-        <div
-          class="flex pb-5 px-3 m-auto pt-5 text-gray-800 pt-8 mb-5 flex-col
-      flex-row max-w-6xl text-center grid grid-cols-11 xl:gap-10 lg:gap-10 md:gap-10 sm:gap-5"
-        >
-          <div class="text-center col-start-2 col-span-8">
-            © Erfan Habibi Panah Fard. All Rights Reserved.
-          </div>
-          <div class={"flex col-end-12 col-span-2"}>
-            <a
-              rel="preconnect"
-              href="https://www.linkedin.com/in/erfanhabibipanah"
-              target="_blank"
-              className={`w-6 mx-1 ${footerStyle.a}`}
-            >
-              <Linkedin />
-            </a>
-            <a
-              rel="preconnect"
-              href="https://github.com/erfanhabibipanah"
-              target="_blank"
-              className={`w-6 mx-1 ${footerStyle.a}`}
-            >
-              <GitHub />
-            </a>
-            <a
-              rel="preconnect"
-              href="https://twitter.com/e_habibipanah"
-              target="_blank"
-              className={`w-6 mx-1 ${footerStyle.a}`}
-            >
-              <TwitterIcon />
-            </a>
-          </div>
+        <div class="text-center ">
+          © Erfan Habibi Panah Fard. All Rights Reserved.
+        </div>
+        <div class={" place-content-center py-5 flex"}>
+          <a
+            rel="preconnect"
+            href="https://www.linkedin.com/in/erfanhabibipanah"
+            target="_blank"
+            className={`w-6 mx-1 ${footerStyle.a}`}
+          >
+            <Linkedin />
+          </a>
+          <a
+            rel="preconnect"
+            href="https://github.com/erfanhabibipanah"
+            target="_blank"
+            className={`w-6 mx-1 ${footerStyle.a}`}
+          >
+            <GitHub />
+          </a>
+          <a
+            rel="preconnect"
+            href="https://twitter.com/e_habibipanah"
+            target="_blank"
+            className={`w-6 mx-1 ${footerStyle.a}`}
+            style={{ marginTop: "-2px" }}
+          >
+            <TwitterIcon style={{ color: "#00acee" }} />
+          </a>
         </div>
       </div>
     </div>
